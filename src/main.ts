cf7f860aa9362f365f8bd22ebe802eb8b877d488
Adil Zhexenov <adilzhexenoff@gmail.com>
2024-07-04 01:33:45 +0500

    init: create a basic structure of project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const { PORT } = process.env;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(PORT || 3000);
+}
+
+bootstrap();
